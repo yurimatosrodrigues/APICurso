a60b438cbb3269fe58c656dd7b670d0bbcea842e
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-03-14 17:17:57 -0300

    Criacao do projeto

---
+++ curso/src/main/java/br/gov/sp/etec/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.etec.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
